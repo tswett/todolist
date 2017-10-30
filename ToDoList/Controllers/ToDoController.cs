f2d3df84c5b1c9b1d66320d4f61b33d474ff9359
Tanner Swett <tannerswett@gmail.com>
2017-10-30 14:00:08 -0400

    Create basic to-do item controller

---
+++ ToDoList/Controllers/ToDoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private ToDoItemDbContext db = new ToDoItemDbContext();
+
+        // GET: ToDo
+        public ActionResult Index()
+        {
+            return View(db.ToDoItems.ToList());
+        }
+
+        // GET: ToDo/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDoItem toDoItem = db.ToDoItems.Find(id);
+            if (toDoItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toDoItem);
+        }
+
+        // GET: ToDo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ToDo/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Content")] ToDoItem toDoItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ToDoItems.Add(toDoItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(toDoItem);
+        }
+
+        // GET: ToDo/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDoItem toDoItem = db.ToDoItems.Find(id);
+            if (toDoItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toDoItem);
+        }
+
+        // POST: ToDo/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Content")] ToDoItem toDoItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(toDoItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(toDoItem);
+        }
+
+        // GET: ToDo/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDoItem toDoItem = db.ToDoItems.Find(id);
+            if (toDoItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toDoItem);
+        }
+
+        // POST: ToDo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ToDoItem toDoItem = db.ToDoItems.Find(id);
+            db.ToDoItems.Remove(toDoItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
