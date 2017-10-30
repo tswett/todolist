7040a3247676e7346ac0cfa859a9a5aabceaf58e
Tanner Swett <tannerswett@gmail.com>
2017-10-30 13:55:39 -0400

    Modify templates, create ToDoItem model class

---
+++ ToDoList/Models/ToDoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class ToDoItem
+    {
+        public int ID { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class ToDoItemDbContext : DbContext
+    {
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+    }
+}
